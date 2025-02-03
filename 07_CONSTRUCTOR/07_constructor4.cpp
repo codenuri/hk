a1500463f3b25a4375a3847b7d452b9b153f6c5c
ecourse <smkang@codenuri.co.kr>
2025-02-03 14:35:01 +0900

    Add files via upload

--- 07_CONSTRUCTOR/07_constructor4.cpp
+++ 07_CONSTRUCTOR/07_constructor4.cpp
@@ -16,11 +16,17 @@ class Rect
 public:
 	Rect()
 	{
+		// 컴파일러가 멤버 데이타의 생성자 호출 코드는 제일 위에 만들어 줍니다.
+		// 항상 인자 없는 생성자 호출
+		// call Point::Point() // leftTop 에 대해서
+		// call Point::Point() // rightBottom 에 대해서
+		
 		std::cout << "Rect()" << std::endl;
 	}
 };
 
 int main()
 {
-	Rect rc; 
+	Rect rc; 	// 1. sizeof(rc) 크기의 메모리를 스택에 할당당
+				// 2. call Rect::Rect() 생성자 호출
 }
