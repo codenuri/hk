a3324794a632af4fd5633e182b42833804048dfa
ecourse <smkang@codenuri.co.kr>
2025-02-03 16:20:31 +0900

    Add files via upload

--- 10_CONST_MEMBER_FUNCTION/10_const_member_function6.cpp
+++ 10_CONST_MEMBER_FUNCTION/10_const_member_function6.cpp
@@ -9,7 +9,10 @@ public:
 	}
 	~vector() { delete[] ptr;}
 
-	int operator[](int idx) { return ptr[idx];}
+	// 핵심 #1. reference 반환을 사용하면 함수 호출이 등호의 왼쪽에 올수 있습니다.
+	// 그래서 "v[0] = 10" 이 가능해 집니다.
+	
+	int& operator[](int idx) { return ptr[idx];}
 };
 int main()
 {
