07259de5008e22602abf15334c4e5593f6c070fc
ecourse <smkang@codenuri.co.kr>
2025-02-03 11:16:32 +0900

    Add files via upload

--- 03_REFERENCE/03_reference1.cpp
+++ 03_REFERENCE/03_reference1.cpp
@@ -6,9 +6,13 @@ int main()
 	int  n = 10;
 
 	int& r = n;		// r은 n의 별명(alias) 입니다.
+
 	r = 30;        	// n = 30 과 같은 의미.
 
 	std::cout << n << std::endl; // 30
+
+	std::cout << &n << std::endl; // 
+	std::cout << &r << std::endl; // 위와 동일한 주소. 
 
 }
 
