7c366514b75ce09973103d098156bedb56e9ef1d
ecourse <smkang@codenuri.co.kr>
2025-02-03 15:02:36 +0900

    Add files via upload

--- 08_MEMBER_INITIALIZER_LIST/08_member_initializer_list3.cpp
+++ 08_MEMBER_INITIALIZER_LIST/08_member_initializer_list3.cpp
@@ -1,5 +1,9 @@
 #include <string>
 
+// std::string 은 alias 입니다. 
+// 실제 이름은 std::basic_string 이라는 템플릿 입니다.
+
+
 class Person1
 {
 	std::string name;
@@ -7,19 +11,31 @@ class Person1
 public:
 	Person1(const std::string& n, int a) 
 	{
-		name = n;	
+		// call std::string::string()  - name에 대해서 
+
+		name = n; // name.operator=(n) 함수 호출	
+				  // 즉, 이코드는 초기화가 아닌 대입입
+
 		age = a;
 	}
 };
+
+// Person1 p1("kim", 20);
+
+
 
 class Person2
 {
 	std::string name;
 	int age;
 public:
+	// 초기화 리스트의 의미
+	// => 멤버 데이타의 생성자 호출하는 코드를 만들때
+	// => 초기화 리스트 모양대로 만들어 달라는 것(디폴트 생성자가 아닌 다른 생성자)
 	Person2(const std::string& n, int a) : name{n}, age{a}
 	{
-
+		// call std::string::string()  이렇게 만들지 않고!!
+		// call std::string::string{n}  이렇게 만들게 된다. {}또는 () 동일
 	}
 };
 
--- 08_MEMBER_INITIALIZER_LIST/08_member_initializer_list5.cpp
+++ 08_MEMBER_INITIALIZER_LIST/08_member_initializer_list5.cpp
@@ -5,18 +5,33 @@ class Point
 	int x;
 	int y;
 public:
-	Point() 			: x{0}, y{0}{} 
+//	Point() 			: x{0}, y{0}{} 
 	Point(int a, int b) : x{a}, y{b}{} 
 };
+
+// 핵심 : Point 의 디폴트 생성자가 없습니다
+//		 아래 코드가 왜 에러인지 정확히 알아야 합니다.
 
 class Rect
 {
 	Point leftTop;
 	Point rightBottom;
 public:
+	// 아래 생성자는 컴파일러가 만드는 아래 코드(주석)때문에 에러
+	/*
 	Rect()
 	{
+		// call Point::Point()
+		// call Point::Point()
 	}
+	*/
+	Rect() : leftTop(0, 0), rightBottom{0, 0}  // 동일방법이 좋은데 
+	{										// 다양한 방법을 보여주기 위해 수업용
+		// call Point::Point(0, 0)
+		// call Point::Point(0, 0)
+	}
+	
+
 };
 
 int main()
