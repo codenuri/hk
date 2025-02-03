f1e90e68edf6e7da54901676b8b5770a6c35f489
ecourse <smkang@codenuri.co.kr>
2025-02-03 15:23:29 +0900

    Add files via upload

--- 09_THIS/09_this2.cpp
+++ 09_THIS/09_this2.cpp
@@ -6,13 +6,12 @@ class Point
 {
 	int x, y;
 public:
-	// #1. 
+	// #1. 이름 충돌시 멤버임을 명확히 나타내기 위해서
 	void set(int x, int y) 
 	{
-		x = x;	
+		this->x = x;	
 
 	}
-
 	// #2. this 를 반환
 	Point* foo() { return this; }
 };
@@ -20,4 +19,11 @@ int main()
 {
 	Point p1;
 	p1.set(10, 20);
+
+	// this를 반환하면 연속적인 함수 호출 가능
+	// => cout 원리
+	p1.foo()->foo()->foo();
+
+	std::cout << "A" << "B" << "C";
+//	std::cout.operator<<("A").operator<<("B").oprator<<("C");
 }
