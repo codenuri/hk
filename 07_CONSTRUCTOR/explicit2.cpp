f1a7074b12fc22ed330805aba33cb1bec7030638
ecourse <smkang@codenuri.co.kr>
2025-02-03 17:03:07 +0900

    Add files via upload

--- 07_CONSTRUCTOR/explicit2.cpp
+++ 07_CONSTRUCTOR/explicit2.cpp
@@ -8,12 +8,14 @@ void f2(std::vector<int> v){}
 int main()
 {
 	// 아래 코드는 되는게 좋은까요 ? 안되는게 좋을까요 ?
-	f1("hello");
-	f2(10);
+	f1("hello");	// ok
+	f2(10);			// error
 
-	std::string s = "hello";
-	std::vector<int> v = 10; 
+	std::string s = "hello";	// ok
+								// std::string(const char*) 생성자는 explicit 아님.
+	std::vector<int> v1(10);  // ok
+	std::vector<int> v2 = 10;  // error. vector(int) 생성자는 explicit 라는 의미미
 
-	std::shared_ptr<int> sp1 = new int;
-	std::shared_ptr<int> sp2( new int);
+	std::shared_ptr<int> sp1 = new int; // error
+	std::shared_ptr<int> sp2( new int); // ok.
 }
