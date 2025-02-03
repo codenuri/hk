dea84c07140e222d58c412b5e180efd8241492dd
ecourse <smkang@codenuri.co.kr>
2025-02-03 10:28:06 +0900

    Add files via upload

--- 02_CASTING/02_casting2.cpp
+++ 02_CASTING/02_casting2.cpp
@@ -4,7 +4,13 @@ int main()
 {
 	const int c = 10;
 
-	int* p = &c;
+//	int* p = &c;	// error
+	int* p = (int*)&c;	// ok. 
 
-	*p = 20;
+	*p = 20; // ok
+
+	// 대부분 컴파일러는 아래 처럼 나오지만, 표준 아닙니다.
+	// 절대 사용하면 안됩니다.
+	std::cout << c << std::endl;  // 10
+	std::cout << *p << std::endl; // 20 
 }
