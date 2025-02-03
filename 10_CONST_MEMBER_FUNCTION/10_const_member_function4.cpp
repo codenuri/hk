91fe1886835fd85a1ab0e2df8eddbbfbdbc076f3
ecourse <smkang@codenuri.co.kr>
2025-02-03 15:55:08 +0900

    Add files via upload

--- 10_CONST_MEMBER_FUNCTION/10_const_member_function4.cpp
+++ 10_CONST_MEMBER_FUNCTION/10_const_member_function4.cpp
@@ -1,24 +1,28 @@
 #include <iostream>
 
+// 상수 함수의 정확한 원리
+
 class Point
 {
 	int x, y;
 public:
-	Point(int a = 0, int b = 0) : x{a}, y{b} {}
+	Point(int a , int b ) : x{a}, y{b} {}
 
-	void set(int a, int b) { x = a; y = b; }
-
-	void print() const
+//	void print() 		// void print(Point* this)
+	void print() const	// void print(const Point* this)
 	{	
+	//	x = 10; // this->x = 10; 에서 this 가 const Point* 일때는 에러
+	
 		std::cout << x << ", " << y << std::endl;
 	}
 }; 
 int main() 
 {
-	Point p(1, 2);
+	Point p1(1, 2);
+	const Point p2(1, 2);
 
-	p.x = 10;		
-	p.set(10, 20);	
-	p.print();		
+	p1.print();	// print(&p1) 입니다.	
+	p2.print();	// print(&p2) 입니다.
+				// print 가 const 가 아니면 "Point* this = &p2" 이므로 에러.
 }
 
