9017585b2b673b3e48fdc900a3e606c9ae2c93b1
ecourse <smkang@codenuri.co.kr>
2025-02-03 10:54:00 +0900

    Add files via upload

--- 02_CASTING/02_casting6.cpp
+++ 02_CASTING/02_casting6.cpp
@@ -1,15 +1,34 @@
 ﻿// const_cast : 상수성 제거를 위한 캐스팅
+//			   => 동일 타입으로만 상수성 제거 가능.
 
 int main()
 {
-	int n = 10;
-	
-	const int* p = &n;
-
-	int* p1 = p;
-	int* p2 = static_cast<int*>(p);	
-
-
 	const int c = 10;
+
+	int* p3 = static_cast<int*>(&c); // error
+	int* p3 = reinterpret_cast<int*>(&c); // error
+	int* p3 = const_cast<int*>(&c); // ok.
+
+	*p3 = 20;	// 절대 이렇게는 사용하면 안됩니다. undefined
+				// 그래서
+				// const_cast 는 원본이 상수인 변수에 대해서는 
+				// 절대 사용하면 안됩니다.
+				// 위 캐스팅이 에러 아니지만 사용하지 마세요
+				// 아래 경우만 사용하세요
+	//-------------------------------------------------
+
+	int n = 10;	// 원본은 상수가 아닌데
+
+	const int* p = &n;	// 가리키는 포인터가 상수
+						// 즉, p를 통해서는 읽기만 하겠다는 의도. 좋은 코드
+
+	int* p1 = p;	// error. 아래 처럼 const_cast 만 가능.
+	int* p2 = static_cast<int*>(p);		//error
+	int* p2 = reinterpret_cast<int*>(p);//error  
+	int* p2 = const_cast<int*>(p); // ok, 이런 경우에 사용하라고 만든 캐스팅
+
+	*p2 = 20;
+
+	
 	
 }
--- 02_CASTING/02_casting7.cpp
+++ 02_CASTING/02_casting7.cpp
@@ -3,7 +3,9 @@
 	const int c = 10;
 
 	// c의 주소를 double* 에 담고 싶습니다.
-	double* p = &c; // ?
+//	double* p = (double*)&c; // C언어는 이렇게 하면 ok
+ 
+	double* p = ? &c; // C++로 해보세요
 
 }
 
