b65fd0bca87502130b031b0b251e43993967c0ce
ecourse <smkang@codenuri.co.kr>
2025-02-03 11:37:17 +0900

    Add files via upload

---
+++ 03_REFERENCE/03_reference4-1.cpp
@@ -0,0 +1,14 @@
+
+// 인자를 수정하지 않고 읽기만 하려고 합니다.
+// 다음중 좋은 것은 ?
+void f1( BigData bd ) {}
+void f2( const BigData& bd ) {}
+
+
+void f3(int n) {}
+void f4(const int& n) {}
+
+int main()
+{
+	
+}
--- 03_REFERENCE/03_reference4.cpp
+++ 03_REFERENCE/03_reference4.cpp
@@ -3,17 +3,30 @@ struct BigData
 	int buff[1024];
 };
 
+
+// call by value : 복사본 생성. 원본 데이타 수정 할수 없음.
+//					함수간 독립성이 생기므로 안전하다!! 아주 좋은 코드. 		
 void foo(int n)
 {
 	n = 20;
 }
-void goo( BigData bd ) 
+
+// 그런데, 인자의 타입이 크기가 크다면 
+// call by value 는 복사본의 오버헤드가 크다.
+//void goo( BigData bd ) 
+
+// optimization : 메모리 복사본을 없애기 위해서 call by reference 
+// safety       : 안전성(함수간 독립)을 위해서 const 
+void goo( const BigData& bd ) 
 {
+//	bd.member = 10; // error
 }
+
 int main()
 {
 	int x = 100;
 	foo(x);
+
 
 	BigData b;
 	goo(b);
