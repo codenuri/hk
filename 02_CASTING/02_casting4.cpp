ab155ec0c32a4f6aea843cde710cbc728c730b02
ecourse <smkang@codenuri.co.kr>
2025-02-03 10:44:14 +0900

    Add files via upload

--- 02_CASTING/02_casting4.cpp
+++ 02_CASTING/02_casting4.cpp
@@ -1,23 +1,42 @@
 ﻿#include <iostream>
 
-// static_cast : 논리적으로 맞고 위험하지 않은 경우만 허용
+// static_cast : 컴파일 시간에 캐스팅 해달라는것
+// dynamic_cast : 실행시간에 캐스팅 해달라는것
+
+// 특수 목적 캐스팅 : reinterpret_cast, const_cast 
+
+// static_cast : 범용적 캐스팅, 일반적인 용도로 사용
+//			논리적으로 맞고 위험하지 않은 경우만 허용
+//			표준 타입끼리의 변환
 //          void*=> 다른 타입*,
 //			상속관계의 캐스팅 등.
 //          반드시 연관성이 있어야 한다
-
 
 int main()
 {
 	double d = 3.4;
 
 	// #1. 표준 타입 끼리의 변환
-	int    n = d;
+//	int    n = d;	// 암시적 변환, 경고 나올수 있음
+	int    n = static_cast<int>(d); // 명시적 변환, 경고 없음
+
+
 
 	// #2. void* => 다른타입* 변환
-	int* p1 = malloc(100);    
+//	int* p1 = malloc(100);    	// C 언어는 암시적 변환도 가능. C++ 안됨
+								// C++ 이면 error
+
+	int* p1 = static_cast<int*>(malloc(100)); // ok
 
 
-	// #3. int* => double* 
-	double* p2 = &n;
+	// #3. 그외 상속관계의 타입캐스팅 가능. 
+
+
+	// #4. int* => double* 등의 연관성없는 타입의 주소 변환
+	double* p2 = (double*)&n; // ok
+	double* p2 = static_cast<double*>(&n); // error
+
+	// 의도 : 위험한 캐스팅은 "해당작업을 위한 전용 캐스팅" 을 사용해서
+	//		  코드에 의도를 나타내라는 것
 }
 
--- 02_CASTING/02_casting5.cpp
+++ 02_CASTING/02_casting5.cpp
@@ -1,7 +1,8 @@
 ﻿#include <iostream>
 
-// reinterpret_cast 
-// 1. 서로 다른 타입 끼리의 주소 변환
+// reinterpret_cast : 메모리에 있는 데이타를 다른 방식으로 해석하겠다.
+//					 그래서 이름이 "reinterpret"
+// 1. 서로 다른 타입 끼리의 주소(참조) 변환
 // 2. 정수 <=> 포인터 사이의 변환
 
 int main()
@@ -9,12 +10,21 @@ int main()
 	int n =  10; 
 
 	// #1. 서로 다른 타입의 주소 변환
-	double* p1 = static_cast<double*>(&n);
-
+//	double* p1 = static_cast<double*>(&n);	// error
+	double* p1 = reinterpret_cast<double*>(&n);	// ok
 
 	// #2. 정수 <=> 포인터 변환
-	int* p2 = 10;
+	int* p2 = 10;	// error
+	int* p2 = (int*)10;	// ok. C 캐스팅은 대부분 가능.
+	int* p2 = static_cast<int*>(10);	// error. 위험한 작업.
+	int* p2 = reinterpret_cast<int*>(10); // ok 
 
-	int  n = p2;
+	// 아래 코드는 포인터를 다시 정수로
+	int  n = reinterpret_cast<int>(p2); // ok
+
+	//--------------------------
+	// reinterpret_cast 는 위 용도위에 다른 용도는 사용 불가
+	int n2 = 3.4; // ok. 단, 경고 나올수 있다.
+	int n2 = reinterpret_cast<int>(3.4); // error. 이 용도가 아니다.
 }
 
