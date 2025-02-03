1d8fee7124dc8cde6e5cddf563b2c3b953ef6c26
ecourse <smkang@codenuri.co.kr>
2025-02-03 11:28:57 +0900

    Add files via upload

--- 03_REFERENCE/03_reference2.cpp
+++ 03_REFERENCE/03_reference2.cpp
@@ -1,36 +1,51 @@
 // pointer vs reference
+
+// 대부분의 컴파일러가 레퍼런스 구현을 내부적으로 포인터 사용합니다
+// => 단, 컴파일러에 따라 다를수 있습니다.
+
+
+// pointer vs reference 차이점.
+
 int main()
 {
 	int  n = 10;
 
-	// #1. 초기화 하는 방법
-	int* p1 = &n;
-	int& r1 = n;
+	// #1. 초기화 하는 방법, & 연산자에 대해서
+
+	int* p1 = &n;	// 주소로 초기화, & 연산자는 "주소연산자"
+	int& r1 = n;	// 변수이름으로 초가화, & 연산자는 "레퍼런스 연산자자"
 
 	// #2. 대상체(n) 에 접근
-	*p1 = 20;
-	r1  = 20;
+	*p1 = 20;	// * 필요
+	r1  = 20;	// * 필요 없음.
 
 	// #3. 
 	int n2 = 20;
 
-	p1 = &n2;
-	r1 = n2;
+	p1 = &n2;	// p1이 가리키는 대상체 변경. 
+	r1 = n2;	// 대상체 변경 아님. n2값을 r1의 대상체에 대입
+				// 레퍼런스는 n1 의 별명으로 만든후에는 변경할수 없다.
 
 	// #4.
-	int* p2;
-	int* p3 = nullptr;
-	int& r2; 
+	int* p2;			// ok. 초기값없어도 된다.
+	int* p3 = nullptr;	// ok. null pointer 도 가능
+
+	int& r2; 	// error.반드시 초기화 필요
+	int& r2 = nullptr; // error. 
+	int& r2 = n2; // ok . 반드시 유효한 대상체 필요
 }
 
 // pointer 보다 reference 가 안전 하다.
 
 void f1(int* p)
 {
-	*p = 10;
+	if ( p != nullptr) // 안전하게 하려면 check 필요!!
+		*p = 10;
 }
 
 void f2(int& r)
 {
-	r = 10;
+	r = 10;	// r은 항상 유효하다.!! check 필요 없다.
 }
+f1(nullptr); // ok
+f2(nullptr); // error. 유효하지 않은 객체를 보낼수 없다.
--- 03_REFERENCE/03_reference3.cpp
+++ 03_REFERENCE/03_reference3.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 
+// 인자의 원본데이타를 1증가하고 싶다.
 void inc1(int n)  { ++n; }
 void inc2(int* p) { ++(*p); }
 void inc3(int& r) { ++r; } 
@@ -8,11 +9,17 @@ int main()
 {
 	int a = 1, b = 1, c = 1;
 
-	inc1( a ); // call by value
+	inc1( a ); // call by value	
 	inc2( &b); // call by pointer
 	inc3( c ); // call by reference
 
-	std::cout << a << std::endl;
-	std::cout << b << std::endl;
-	std::cout << c << std::endl;
+	std::cout << a << std::endl;	// 1
+	std::cout << b << std::endl;	// 2
+	std::cout << c << std::endl;	// 2
 }
+
+// 레퍼런스를 함수 인자로 사용하면
+// 포인터 처럼 원본 데이타를 수정할수 있습니다.
+// => 레퍼런스 권장
+// 장점 1. 가독성(간결하다)
+// 장점 2. safety(안전하다.)
