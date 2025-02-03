2c4e142ddbe517df6d9066c84e17987d0e86256a
ecourse <smkang@codenuri.co.kr>
2025-02-03 13:04:03 +0900

    Add files via upload

--- 03_REFERENCE/03_reference6.cpp
+++ 03_REFERENCE/03_reference6.cpp
@@ -2,12 +2,14 @@ int main()
 {
 	int n = 0;
 
-	n = 3;
-	3 = n;
+	n = 3;	// ok.    n 은 lvalue : 등호의 왼쪽에 올수 있는 것
+	3 = n;	// error. 3 은 rvalue
 
-	int& r1 = n;
-	int& r2 = 3;
+	int& r1 = n;	// ok
+	int& r2 = 3;	// error. rvalue 를 reference 로 가리킬수 없다.
 
-	int&& r3 = n;
-	int&& r4 = 3;
+	// C++11 의 rvalue reference 문법 - 수요일부터 시작.. 
+	// => move 와 perfect forwarding 에서 자세히 설명.
+	int&& r3 = n;	// error
+	int&& r4 = 3;	// ok
 }
