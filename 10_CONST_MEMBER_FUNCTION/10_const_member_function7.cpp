89cad62ddd0af52bce9484e5152db9b7dc2c47f5
ecourse <smkang@codenuri.co.kr>
2025-02-03 16:23:01 +0900

    Add files via upload

---
+++ 10_CONST_MEMBER_FUNCTION/10_const_member_function7.cpp
@@ -0,0 +1,26 @@
+class vector
+{
+	int* ptr;
+	int size;
+public:
+	vector(int sz) : size(sz)
+	{
+		ptr = new int[sz];
+	}
+	~vector() { delete[] ptr;}
+
+	int& operator[](int idx) { return ptr[idx];}
+};
+int main()
+{
+	      vector v1(10); // 10 개 배열처럼 사용하겠다는 것
+	const vector v2(10); // 10 개 상수 배열처럼 사용하겠다는 것 
+
+	// 아래 ?를 O(되야한다)/X(에러나와야 한다) 로 채워 보세요
+	//					// 원하는 동작		현재 상태
+	v1[0] = 10;			//	?				?
+	v2[0] = 10;			// 	?				?
+
+	int n1 = v1[0];		//	?				?
+	int n2 = v2[0];		//	?				?
+}
