4887110a3ae0aadd1787bb67864a6cb44c79f5e9
ecourse <smkang@codenuri.co.kr>
2025-02-03 15:54:35 +0900

    Add files via upload

--- 10_CONST_MEMBER_FUNCTION/10_const_member_function5.cpp
+++ 10_CONST_MEMBER_FUNCTION/10_const_member_function5.cpp
@@ -1,19 +1,38 @@
 #include <string>
+#include <iostream>
 
 class Point 
 {
 	int x, y;
+	// mutable 멤버 데이타 : 상수 멤버 함수안에서도  변경가능한 멤버 만들때 사용.
+	mutable std::string cache;
+	mutable bool cache_valid = false;
 public:
 	Point(int a, int b) : x{a}, y{b} {}
 
-	std::string to_string()
+	// 객체의 상태를 문자열로 반환하는 함수 1, 2 => "1, 2"
+	std::string to_string() const
 	{
-		std::string s = std::to_string(x) + ", " + std::to_string(y);
+//		std::string s = std::to_string(x) + 
+//						", " + std::to_string(y);
+
+		if( cache_valid == false )
+		{
+			cache = std::to_string(x) + ", " + std::to_string(y);	
+			cache_valid = true;
+		}
+
+		return cache;
 	}
 };
 
 int main()
 {
+//	const Point p(1,2);
+
 	Point p(1,2);
+
+	std::cout << p.to_string() << std::endl;
 	std::cout << p.to_string() << std::endl;
 }
+	
