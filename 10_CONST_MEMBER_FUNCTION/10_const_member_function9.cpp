442666d7ab9d5e804a239a664dacdf042ca26772
ecourse <smkang@codenuri.co.kr>
2025-02-03 16:33:12 +0900

    Add files via upload

--- 10_CONST_MEMBER_FUNCTION/10_const_member_function9.cpp
+++ 10_CONST_MEMBER_FUNCTION/10_const_member_function9.cpp
@@ -12,16 +12,19 @@ public:
 	int& operator[](int idx)       { return ptr[idx];}
 	int  operator[](int idx) const { return ptr[idx];}
 };
-
 int main()
 {
+//	const std::vector<int> v3 = {1,2,3};
+//	v3[0] = 10; // error
+//	int n = v3[0]; // ok
+
 	      vector v1(10); 
 	const vector v2(10); 
 
 
 	//					// 원하는 동작		현재 상태
 	v1[0] = 10;			//	O				O
-	v2[0] = 10;			// 	X				O
+	v2[0] = 10;			// 	X				X
 
 	int n1 = v1[0];		//	O				O
 	int n2 = v2[0];		//	O				O
